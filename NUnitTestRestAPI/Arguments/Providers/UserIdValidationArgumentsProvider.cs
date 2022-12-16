142076430b5471200e1b482c97534d803de1e5f3
MEJamnezhad <e.jamnezhad@gmail.com>
2022-12-15 18:31:09 -0700

    Add project files.

---
+++ NUnitTestRestAPI/Arguments/Providers/UserIdValidationArgumentsProvider.cs
@@ -0,0 +1,47 @@
+﻿using NUnitTestRestAPI.Arguments.Holders;
+using RestSharp;
+using System.Net;
+using System.Collections;
+
+
+namespace NUnitTestRestAPI.Arguments.Providers
+{
+    public class UserIdValidationArgumentsProvider : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new ValidationArgumentHolder
+                {
+                    ErrorMessage="",
+                    StatusCode = HttpStatusCode.NotFound,
+                    PathParameters = Parameter.CreateParameter("Id",-10,ParameterType.UrlSegment)
+                }
+            };
+
+            yield return new object[]
+           {
+                new ValidationArgumentHolder
+                {
+                    ErrorMessage="",
+                    StatusCode = HttpStatusCode.NotFound,
+                    PathParameters = Parameter.CreateParameter("Id",int.MinValue,ParameterType.UrlSegment)
+                }
+           };
+
+            for (int i = 1; i < 20; i++)
+            {
+                yield return new object[]     
+                {
+                    new ValidationArgumentHolder
+                    {
+                        ErrorMessage="",
+                        StatusCode = HttpStatusCode.OK,
+                        PathParameters = Parameter.CreateParameter("Id",i,ParameterType.UrlSegment)
+                    }
+                };
+            }
+        }
+    }
+}
---
+++ NUnitTestRestAPI/UserTest.cs
@@ -0,0 +1,93 @@
+﻿using NUnit.Framework;
+using NUnitTestRestAPI.Arguments.Holders;
+using NUnitTestRestAPI.Arguments.Providers;
+using RestSharp;
+using System.Net;
+
+namespace NUnitTestRestAPI
+{
+    public class UserTest
+    {
+        private static RestClient client;
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            client = new RestClient(@"http://127.0.0.1:5186/api/");
+        }
+
+
+        [Test]
+        public void TestGetAllUsersWithpagination()
+        {
+            RestRequest request = new RestRequest("users");
+            request.AddQueryParameter("size", "0");
+            request.AddQueryParameter("page", "1");
+            Console.WriteLine($"{request.Method} {client.Options.BaseUrl}{request.Resource}");
+            var response = client.Get(request);
+            Assert.IsNotNull(response);
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Test]
+        public void TestGetAllUsers()
+        {
+            RestRequest request = new RestRequest("users/all");
+            Console.WriteLine($"{request.Method} {client.Options.BaseUrl}{request.Resource}");
+            var response = client.Get(request);
+            Assert.IsNotNull(response);
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Test]
+        public void TestGetByRandomId()
+        {
+            RestRequest request = new RestRequest("users/{Id}");
+            Random random = new Random();
+
+            int rand = random.Next(-10, int.MaxValue);
+
+
+            request.AddUrlSegment("Id", rand);
+            var response = client.Get(request);
+
+            Assert.IsNotNull(response);
+            Assert.That((HttpStatusCode.OK == response.StatusCode) || (HttpStatusCode.NotFound == response.StatusCode));
+            // Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+
+            Console.WriteLine($"{request.Method} {response.StatusCode.ToString()} {response.ResponseUri}");
+
+
+        }
+
+
+        [Test]
+        public void TestGetByError()
+        {
+            RestRequest request = new RestRequest("users/{Id}");
+
+            request.AddUrlSegment("Id", -10);
+            var response = client.Get(request);
+
+            Assert.IsNotNull(response);
+            Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
+            Console.WriteLine($"{request.Method} {response.StatusCode.ToString()} {response.ResponseUri}");
+        }
+
+
+        // 
+        [Test]
+        [TestCaseSource(typeof(UserIdValidationArgumentsProvider))]
+        public void TestGetIDByError(ValidationArgumentHolder validationArgument)
+        {
+            RestRequest request = new RestRequest("users/{Id}");
+
+            request.AddOrUpdateParameter(validationArgument.PathParameters);
+            var response = client.Get(request);
+
+            Assert.IsNotNull(response);
+            Assert.That(validationArgument.StatusCode == response.StatusCode);
+            Console.WriteLine($"{request.Method} {response.StatusCode.ToString()} {response.ResponseUri}");
+        }
+
+    }
+}
